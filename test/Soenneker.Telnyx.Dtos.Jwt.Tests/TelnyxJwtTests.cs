c96afdaaabd7d50ce6b6407307e79bce28c42b75
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-23 10:46:00 -0500

    tunit

--- test/Soenneker.Telnyx.Dtos.Jwt.Tests/TelnyxJwtTests.cs
+++ test/Soenneker.Telnyx.Dtos.Jwt.Tests/TelnyxJwtTests.cs
@@ -1,11 +1,10 @@
-﻿using Soenneker.Tests.Unit;
-using Xunit;
+using Soenneker.Tests.Unit;
 
 namespace Soenneker.Telnyx.Dtos.Jwt.Tests;
 
 public sealed class TelnyxJwtTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
