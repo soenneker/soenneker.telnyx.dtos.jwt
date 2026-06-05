733d2d953b38a60bc5e865fb9a1e2c8a27b0080b
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:52 -0500

    xml commenting

--- src/Soenneker.Telnyx.Dtos.Jwt/TelnyxJwt.cs
+++ src/Soenneker.Telnyx.Dtos.Jwt/TelnyxJwt.cs
@@ -7,30 +7,57 @@ namespace Soenneker.Telnyx.Dtos.Jwt;
 /// </summary>
 public sealed class TelnyxJwt
 {
+    /// <summary>
+    /// Gets or sets aud.
+    /// </summary>
     [JsonPropertyName("aud")]
     public string Aud { get; set; } = null!;
 
+    /// <summary>
+    /// Gets or sets exp.
+    /// </summary>
     [JsonPropertyName("exp")]
     public long Exp { get; set; }
 
+    /// <summary>
+    /// Gets or sets iat.
+    /// </summary>
     [JsonPropertyName("iat")]
     public long Iat { get; set; }
 
+    /// <summary>
+    /// Gets or sets iss.
+    /// </summary>
     [JsonPropertyName("iss")]
     public string Iss { get; set; } = null!;
 
+    /// <summary>
+    /// Gets or sets jti.
+    /// </summary>
     [JsonPropertyName("jti")]
     public string Jti { get; set; } = null!;
 
+    /// <summary>
+    /// Gets or sets nbf.
+    /// </summary>
     [JsonPropertyName("nbf")]
     public long Nbf { get; set; }
 
+    /// <summary>
+    /// Gets or sets sub.
+    /// </summary>
     [JsonPropertyName("sub")]
     public string Sub { get; set; } = null!;
 
+    /// <summary>
+    /// Gets or sets tel token.
+    /// </summary>
     [JsonPropertyName("tel_token")]
     public string TelToken { get; set; } = null!;
 
+    /// <summary>
+    /// Gets or sets typ.
+    /// </summary>
     [JsonPropertyName("typ")]
     public string Typ { get; set; } = null!;
 }
